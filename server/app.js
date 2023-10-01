3c9e12ab8053e47bf2d41114c6b0eda4e4de905c
Sathvik777 <katam.sathvik@gmail.com>
2023-10-01 12:16:07 +0200

    Add skeleton

--- server/app.js
+++ server/app.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { json } from 'express';
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
+// Middleware to parse JSON data
+app.use(json());
+
+// Temporary in-memory storage for books (replace with a database in production)
+const books = [];
+
+// Routes
+
+app.get('/health', (req, res) => {
     res.send('Hello World!')
 })
 
+// Get all books
+app.get('/books', (req, res) => {
+    // check id in req
+    // if id exists, return book with that id
+    // else return all books
+    if (req.query.id) {
+        const book = books.find((book) => book.id === req.query.id);
+        res.json(book);
+    } if (req.query.q) {
+        const book = books.find((book) => book.title.includes(req.query.q));
+        res.json(book);
+    } else {
+        res.json(books);
+    }
+});
+
+// Create a new book
+app.post('/books', (req, res) => {
+    const newBook = req.body;
+    books.push(newBook);
+    res.status(201).json(newBook);
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
